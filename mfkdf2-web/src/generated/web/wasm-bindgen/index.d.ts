edbe6bef47f8ac820cdccd6c819b59be0cdf16ae
lonerapier <lonerapier@proton.me>
2025-09-12 23:41:57 +0530

    bindings cleanup

---
+++ mfkdf2-web/src/generated/web/wasm-bindgen/index.d.ts
@@ -0,0 +1,241 @@
+/* tslint:disable */
+/* eslint-disable */
+export function ubrn_ffi_mfkdf2_rust_future_cancel_void(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_free_u64(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_complete_rust_buffer(handle: bigint, f_status_: RustCallStatus): Uint8Array;
+export function ubrn_uniffi_mfkdf2_checksum_func_hotp(): number;
+export function ubrn_ffi_mfkdf2_rust_future_free_u8(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_poll_i32(handle: bigint, callback: any, callback_data: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_complete_pointer(handle: bigint, f_status_: RustCallStatus): bigint;
+export function ubrn_ffi_mfkdf2_rust_future_cancel_i32(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_cancel_u64(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_cancel_u8(handle: bigint): void;
+export function ubrn_uniffi_mfkdf2_fn_func_key(factors: Uint8Array, options: Uint8Array): bigint;
+export function ubrn_ffi_mfkdf2_rust_future_cancel_f64(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_free_i8(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_complete_void(handle: bigint, f_status_: RustCallStatus): void;
+export function ubrn_ffi_mfkdf2_rust_future_poll_u16(handle: bigint, callback: any, callback_data: bigint): void;
+export function ubrn_uniffi_mfkdf2_fn_func_setup_password(password: Uint8Array, options: Uint8Array, f_status_: RustCallStatus): Uint8Array;
+export function ubrn_ffi_mfkdf2_rust_future_complete_u8(handle: bigint, f_status_: RustCallStatus): number;
+export function ubrn_ffi_mfkdf2_rust_future_poll_f64(handle: bigint, callback: any, callback_data: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_cancel_i64(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_complete_i16(handle: bigint, f_status_: RustCallStatus): number;
+export function ubrn_ffi_mfkdf2_rust_future_complete_i8(handle: bigint, f_status_: RustCallStatus): number;
+export function ubrn_ffi_mfkdf2_rust_future_complete_i64(handle: bigint, f_status_: RustCallStatus): bigint;
+export function ubrn_ffi_mfkdf2_rust_future_complete_u64(handle: bigint, f_status_: RustCallStatus): bigint;
+export function ubrn_ffi_mfkdf2_rust_future_free_i16(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_complete_f64(handle: bigint, f_status_: RustCallStatus): number;
+export function ubrn_uniffi_mfkdf2_checksum_func_key(): number;
+export function ubrn_ffi_mfkdf2_rust_future_cancel_i16(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_free_i64(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_poll_void(handle: bigint, callback: any, callback_data: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_free_f64(handle: bigint): void;
+export function ubrn_uniffi_mfkdf2_checksum_func_derive_key(): number;
+export function ubrn_ffi_mfkdf2_rust_future_poll_f32(handle: bigint, callback: any, callback_data: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_poll_u64(handle: bigint, callback: any, callback_data: bigint): void;
+export function ubrn_uniffi_mfkdf2_fn_func_derive_password(password: Uint8Array, f_status_: RustCallStatus): Uint8Array;
+export function ubrn_uniffi_mfkdf2_fn_func_hotp(code: number, f_status_: RustCallStatus): Uint8Array;
+export function ubrn_ffi_mfkdf2_rust_future_free_rust_buffer(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_free_void(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_poll_i16(handle: bigint, callback: any, callback_data: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_free_pointer(handle: bigint): void;
+export function ubrn_uniffi_mfkdf2_checksum_func_setup_password(): number;
+export function ubrn_ffi_mfkdf2_rust_future_complete_f32(handle: bigint, f_status_: RustCallStatus): number;
+export function ubrn_ffi_mfkdf2_rust_future_cancel_f32(handle: bigint): void;
+export function ubrn_uniffi_mfkdf2_fn_func_derive_key(policy: Uint8Array, factors: Uint8Array): bigint;
+export function ubrn_ffi_mfkdf2_rust_future_free_u16(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_free_i32(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_poll_i8(handle: bigint, callback: any, callback_data: bigint): void;
+export function ubrn_ffi_mfkdf2_uniffi_contract_version(): number;
+export function ubrn_ffi_mfkdf2_rust_future_poll_i64(handle: bigint, callback: any, callback_data: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_cancel_rust_buffer(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_poll_u32(handle: bigint, callback: any, callback_data: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_free_f32(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_cancel_i8(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_complete_u32(handle: bigint, f_status_: RustCallStatus): number;
+export function ubrn_ffi_mfkdf2_rust_future_complete_u16(handle: bigint, f_status_: RustCallStatus): number;
+export function ubrn_ffi_mfkdf2_rust_future_poll_u8(handle: bigint, callback: any, callback_data: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_poll_rust_buffer(handle: bigint, callback: any, callback_data: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_cancel_u16(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_cancel_u32(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_free_u32(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_cancel_pointer(handle: bigint): void;
+export function ubrn_ffi_mfkdf2_rust_future_complete_i32(handle: bigint, f_status_: RustCallStatus): number;
+export function ubrn_ffi_mfkdf2_rust_future_poll_pointer(handle: bigint, callback: any, callback_data: bigint): void;
+export function ubrn_uniffi_mfkdf2_checksum_func_derive_password(): number;
+export class RustCallStatus {
+  free(): void;
+  constructor();
+  code: number;
+  get errorBuf(): Uint8Array | undefined;
+  set errorBuf(value: Uint8Array | null | undefined);
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly ffi_mfkdf2_rust_future_complete_i8: (a: bigint, b: number) => number;
+  readonly ubrn_ffi_mfkdf2_rust_future_cancel_f32: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_complete_i32: (a: bigint, b: number) => number;
+  readonly ffi_mfkdf2_rust_future_complete_i64: (a: bigint, b: number) => bigint;
+  readonly ffi_mfkdf2_rust_future_free_void: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_poll_rust_buffer: (a: bigint, b: any, c: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_free_f32: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_cancel_i32: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_complete_i32: (a: bigint, b: number) => number;
+  readonly ffi_mfkdf2_rust_future_cancel_f64: (a: bigint) => void;
+  readonly uniffi_mfkdf2_fn_func_derive_password: (a: number, b: number, c: number) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_cancel_i64: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_complete_void: (a: bigint, b: number) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_complete_rust_buffer: (a: bigint, b: number) => [number, number];
+  readonly ubrn_ffi_mfkdf2_rust_future_cancel_u8: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_cancel_u64: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_poll_i32: (a: bigint, b: any, c: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_free_u32: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_free_i8: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_complete_pointer: (a: bigint, b: number) => bigint;
+  readonly ffi_mfkdf2_rust_future_poll_u8: (a: bigint, b: number, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_free_pointer: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_complete_f32: (a: bigint, b: number) => number;
+  readonly ubrn_ffi_mfkdf2_rust_future_free_u16: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_free_u8: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_complete_u8: (a: bigint, b: number) => number;
+  readonly uniffi_mfkdf2_checksum_func_derive_key: () => number;
+  readonly ubrn_uniffi_mfkdf2_fn_func_derive_key: (a: number, b: number, c: number, d: number) => bigint;
+  readonly ffi_mfkdf2_rust_future_cancel_pointer: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_poll_i64: (a: bigint, b: any, c: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_free_pointer: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_cancel_i8: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_poll_i32: (a: bigint, b: number, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_cancel_void: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_free_i32: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_free_u8: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_poll_f32: (a: bigint, b: number, c: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_poll_rust_buffer: (a: bigint, b: number, c: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_poll_pointer: (a: bigint, b: number, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_complete_u16: (a: bigint, b: number) => number;
+  readonly ffi_mfkdf2_rust_future_free_i16: (a: bigint) => void;
+  readonly uniffi_mfkdf2_checksum_func_hotp: () => number;
+  readonly ubrn_ffi_mfkdf2_rust_future_free_i64: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_complete_u64: (a: bigint, b: number) => bigint;
+  readonly ffi_mfkdf2_rust_future_cancel_f32: (a: bigint) => void;
+  readonly uniffi_mfkdf2_fn_func_derive_key: (a: number, b: number) => bigint;
+  readonly ffi_mfkdf2_rust_future_free_i64: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_poll_u32: (a: bigint, b: number, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_cancel_u16: (a: bigint) => void;
+  readonly ubrn_uniffi_mfkdf2_checksum_func_derive_password: () => number;
+  readonly ffi_mfkdf2_rust_future_cancel_i32: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_poll_i16: (a: bigint, b: number, c: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_free_u64: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_cancel_u8: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_cancel_i64: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_poll_f64: (a: bigint, b: any, c: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_cancel_rust_buffer: (a: bigint) => void;
+  readonly ubrn_uniffi_mfkdf2_fn_func_derive_password: (a: number, b: number, c: number) => [number, number];
+  readonly ubrn_ffi_mfkdf2_rust_future_poll_void: (a: bigint, b: any, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_free_rust_buffer: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_complete_u32: (a: bigint, b: number) => number;
+  readonly ffi_mfkdf2_rust_future_cancel_i8: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_complete_void: (a: bigint, b: number) => void;
+  readonly ffi_mfkdf2_rust_future_poll_i8: (a: bigint, b: number, c: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_poll_u16: (a: bigint, b: number, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_free_f64: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_complete_f64: (a: bigint, b: number) => number;
+  readonly ffi_mfkdf2_rust_future_poll_i64: (a: bigint, b: number, c: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_poll_f64: (a: bigint, b: number, c: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_free_rust_buffer: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_poll_i8: (a: bigint, b: any, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_poll_f32: (a: bigint, b: any, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_free_i16: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_complete_f32: (a: bigint, b: number) => number;
+  readonly ffi_mfkdf2_rust_future_complete_u64: (a: bigint, b: number) => bigint;
+  readonly ubrn_uniffi_mfkdf2_fn_func_setup_password: (a: number, b: number, c: number, d: number, e: number) => [number, number];
+  readonly ubrn_uniffi_mfkdf2_fn_func_key: (a: number, b: number, c: number, d: number) => bigint;
+  readonly ubrn_ffi_mfkdf2_rust_future_cancel_i16: (a: bigint) => void;
+  readonly uniffi_mfkdf2_fn_func_key: (a: number, b: number) => bigint;
+  readonly ubrn_ffi_mfkdf2_rust_future_cancel_pointer: (a: bigint) => void;
+  readonly ubrn_uniffi_mfkdf2_checksum_func_hotp: () => number;
+  readonly ubrn_uniffi_mfkdf2_checksum_func_setup_password: () => number;
+  readonly ubrn_ffi_mfkdf2_rust_future_poll_u16: (a: bigint, b: any, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_free_u32: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_cancel_rust_buffer: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_complete_f64: (a: bigint, b: number) => number;
+  readonly uniffi_mfkdf2_checksum_func_derive_password: () => number;
+  readonly ffi_mfkdf2_rust_future_cancel_u32: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_complete_u16: (a: bigint, b: number) => number;
+  readonly ubrn_ffi_mfkdf2_rust_future_poll_u64: (a: bigint, b: any, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_uniffi_contract_version: () => number;
+  readonly ffi_mfkdf2_rust_future_cancel_u16: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_free_f32: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_free_void: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_complete_pointer: (a: bigint, b: number) => number;
+  readonly ubrn_ffi_mfkdf2_rust_future_complete_i64: (a: bigint, b: number) => bigint;
+  readonly ffi_mfkdf2_rust_future_complete_u8: (a: bigint, b: number) => number;
+  readonly uniffi_mfkdf2_checksum_func_key: () => number;
+  readonly ubrn_uniffi_mfkdf2_checksum_func_key: () => number;
+  readonly ffi_mfkdf2_rust_future_free_u16: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_poll_i16: (a: bigint, b: any, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_poll_u32: (a: bigint, b: any, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_poll_u8: (a: bigint, b: any, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_complete_i8: (a: bigint, b: number) => number;
+  readonly uniffi_mfkdf2_fn_func_setup_password: (a: number, b: number, c: number, d: number) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_free_u64: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_poll_u64: (a: bigint, b: number, c: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_poll_pointer: (a: bigint, b: any, c: bigint) => void;
+  readonly uniffi_mfkdf2_checksum_func_setup_password: () => number;
+  readonly ffi_mfkdf2_rust_future_complete_rust_buffer: (a: number, b: bigint, c: number) => void;
+  readonly ffi_mfkdf2_rust_future_free_i8: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_cancel_u64: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_cancel_i16: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_poll_void: (a: bigint, b: number, c: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_cancel_void: (a: bigint) => void;
+  readonly ubrn_uniffi_mfkdf2_fn_func_hotp: (a: number, b: number) => [number, number];
+  readonly ubrn_ffi_mfkdf2_rust_future_complete_i16: (a: bigint, b: number) => number;
+  readonly uniffi_mfkdf2_fn_func_hotp: (a: number, b: number, c: number) => void;
+  readonly ffi_mfkdf2_rust_future_free_f64: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_cancel_f64: (a: bigint) => void;
+  readonly ffi_mfkdf2_rust_future_complete_i16: (a: bigint, b: number) => number;
+  readonly ffi_mfkdf2_uniffi_contract_version: () => number;
+  readonly ubrn_ffi_mfkdf2_rust_future_complete_u32: (a: bigint, b: number) => number;
+  readonly ubrn_uniffi_mfkdf2_checksum_func_derive_key: () => number;
+  readonly ubrn_ffi_mfkdf2_rust_future_cancel_u32: (a: bigint) => void;
+  readonly ubrn_ffi_mfkdf2_rust_future_free_i32: (a: bigint) => void;
+  readonly rustcallstatus_error_buf: (a: number) => [number, number];
+  readonly rustcallstatus_set_error_buf: (a: number, b: number, c: number) => void;
+  readonly rustcallstatus_new: () => number;
+  readonly __wbg_set_rustcallstatus_code: (a: number, b: number) => void;
+  readonly __wbg_get_rustcallstatus_code: (a: number) => number;
+  readonly __wbg_rustcallstatus_free: (a: number, b: number) => void;
+  readonly ffi_mfkdf2_rustbuffer_from_bytes: (a: number, b: number, c: number) => void;
+  readonly ffi_mfkdf2_rustbuffer_reserve: (a: number, b: number, c: bigint, d: number) => void;
+  readonly ffi_mfkdf2_rustbuffer_free: (a: number, b: number) => void;
+  readonly ffi_mfkdf2_rustbuffer_alloc: (a: number, b: bigint, c: number) => void;
+  readonly __wbindgen_exn_store: (a: number) => void;
+  readonly __externref_table_alloc: () => number;
+  readonly __wbindgen_export_2: WebAssembly.Table;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
