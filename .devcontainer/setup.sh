6b92bc4abecf83cc7afeaee30484c5cb194c8e53
Gary Hunt <gary.hunt@datadeliverynetwork.com>
2026-04-11 11:52:50 +0000

    found fix for uniffi installation

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,11 +1,10 @@
 # install just
 cargo install just
+cargo install uniffi --features="cli" # this may have changed since the documentation is cargo install uniffi-bindgen no longer works
 
 # dependencies
-just install-wasm-opt # not done by above
+just install-wasm-opt # not done by setup
 just setup
 # just install-tools # done by above
 # just install-rust # done by above
 # just install-uniffi-deps # done by above
-
-
