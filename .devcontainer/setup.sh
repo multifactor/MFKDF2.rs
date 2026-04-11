265d5e22c18f4f0f97de8f67d473a8b8add6d756
Gary Hunt <gary.hunt@datadeliverynetwork.com>
2026-04-11 11:38:54 +0000

    initial dev container and setup script

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,11 @@
+# install just
+cargo install just
+
+# dependencies
+just install-wasm-opt # not done by above
+just setup
+# just install-tools # done by above
+# just install-rust # done by above
+# just install-uniffi-deps # done by above
+
+
